28ef0632afd47dd64caf72affb4bc32f91cebb6a
brlaney42 <brlaney@outlook.com>
2022-02-02 12:00:48 -0600

    Pushing lunchtime changes

--- test.py
+++ test.py
@@ -36,7 +36,7 @@ import pandas as pd
 data = pd.DataFrame({'x': [0, 10, 20, 30],
                      'y': [0, 10, 10, 0]})
 
-chart = alt.Chart(data).mark_line(point=True).encode(
+chart = alt.Chart(data).mark_circle(point=True).encode(
     alt.X('x', scale=alt.Scale(zero=True)),
     alt.Y('y', scale=alt.Scale(zero=True)),
 )
