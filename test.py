d255a53b16b288a45ea9401a40e0848f8159718d
brlaney42 <brlaney@outlook.com>
2022-02-02 11:29:44 -0600

    added jupyter notebook for testing plots

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
+'''
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
-# df = pd.DataFrame(dict(
-#     x = [0, 10, 20, 30],
-#     y = [0, 10, 10, 0]
-# ))
-# fig = px.line(df, x="x", y="y", title="Unsorted Input")
-# fig.show()
+df = pd.DataFrame(dict(
+    x = [0, 10, 20, 30],
+    y = [0, 10, 10, 0]
+))
+fig = px.line(df, x="x", y="y", title="Unsorted Input")
+fig.show()
 
-# df = df.sort_values(by="x")
-# fig = px.line(df, x="x", y="y", title="Sorted Input")
-# fig.show()
+df = df.sort_values(by="x")
+fig = px.line(df, x="x", y="y", title="Sorted Input")
+fig.show()
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(
@@ -26,3 +27,18 @@ fig.add_trace(go.Scatter(
     name='markers'))
 
 fig.show()
+'''
+
+import altair as alt
+# from vega_datasets import data
+import pandas as pd
+
+data = pd.DataFrame({'x': [0, 10, 20, 30],
+                     'y': [0, 10, 10, 0]})
+
+chart = alt.Chart(data).mark_line(point=True).encode(
+    alt.X('x', scale=alt.Scale(zero=True)),
+    alt.Y('y', scale=alt.Scale(zero=True)),
+)
+
+chart.show()
