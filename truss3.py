8d0780374b86cc9514c76a9c6d04dd6d63f7e760
brlaney42 <brlaney@outlook.com>
2022-02-19 21:27:15 -0600

    Pushing changes

--- truss3.py
+++ truss3.py
@@ -1,5 +1,4 @@
-# Chapter 2 lesson in textbook
-# Reference truss_example_1.png
+# truss example 3
 # import pandas as pd
 from lib.trusses import *
 import numpy as np
@@ -7,23 +6,21 @@ import time
 
 start_time = time.time() # Starting time
 
-
 # Node coordinates
 nodes = np.array([
     [0, 0],
-    [120, 120],
-    [240, 120],
-    [360, 0]
-])
+    [240, 480],
+    [480, 0],
+    [240, 180]])
 
 # Member connection matrix
 members = np.array([
     [1, 2],
-    [2, 3],
-    [3, 4],
+    [1, 4],
     [1, 3],
-    [2, 4]
-])
+    [4, 2],
+    [2, 3],
+    [4, 3]])
 
 L1 = []                       # length in inches
 L2 = []                       # length in feet
@@ -31,23 +28,34 @@ a1 = []                       # units: degrees
 a2 = []                       # units: radians
 n = len(nodes)                # number of nodes
 m = len(members)              # number of members
-A = np.repeat(2, m)           # Cross-sectional areas of each element
-E = np.repeat(29*10**6, m)    # Modulus of elasticity for each element
+A = np.repeat(1, m)           # Cross-sectional areas of each element
+E = np.repeat(1, m)           # Modulus of elasticity for each element
 
 # External forces (lbs) in form: [global dof - 1, (+/-) value]
-fg = np.array([3, -30000])
-# 1 => Un-restrained global degrees of freedom
-dgf = np.array([0, 0, 1, 1, 1, 1, 0, 0])
-# given displacements:
-dp = np.array([ [1, -0.6], [6, -0.3]])
+fg = np.array([[2,-50], [3,25]])
 
-Kg = np.zeros((2*n, 2*n))  # global stiffness matrix
+# 1 => Un-restrained global degrees of freedom
+dgf = np.array([0,0,1,1,1,0,1,1])
+dp = np.array([[0,-0.01], [1,-0.025]])  # given displacements (in)
+Kg = np.zeros((2*n,2*n))  # global stiffness matrix
 Kl = []  # Will contain each elems local [k] (global coords)
+
 
 newKg = KgTruss(n, m, nodes, members, E, A, L1,
         L2, a1, a2, Kg, Kl, fg, dgf)
 
-# print(newKg)
+
+print('\nLength in inches')
+print(L1)
+print('\nLength in feet')
+print(L2)
+print('\nAngles in degrees')
+print(a1)
+print('\nAngles in radians')
+print(a2)
+
+print('\nGlobal stiffness matrix')
+print(newKg)
 
 end_time = time.time() # End time when code finishes
 final_time = end_time - start_time
